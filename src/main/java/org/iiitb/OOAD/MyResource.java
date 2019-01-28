709ae9b6815948ef96e54d8da1e53e9ca8d8060b
Avi K <kushagrakhare@rocketmail.com>
2019-01-28 22:43:46 +0530

    Added addUser and updateUser in DatabaseConnection

--- src/main/java/org/iiitb/OOAD/MyResource.java
+++ src/main/java/org/iiitb/OOAD/MyResource.java
@@ -4,7 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import java.io.InputStream;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.JSONObject;
 
 /**
@@ -24,7 +31,7 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getUserInfo(String user) throws Exception {
         JSONObject user_d = new JSONObject(user);
-        String user_name = user_d.getString("username");
+        String user_name = user_d.getString("user_name");
         
         DatabaseConnection dc = new DatabaseConnection();
         return dc.authenticateUser(user_name);
@@ -39,5 +46,22 @@ public class MyResource {
 	    	
 	    	DatabaseConnection dc = new DatabaseConnection();
 	    	return dc.getItemDetails(item_id);
+	    }
+		
+		@Path("addUser")
+	    @POST
+	    @Consumes(MediaType.MULTIPART_FORM_DATA)
+	    @Produces(MediaType.TEXT_PLAIN)
+	    public String addUser(
+	    		@FormDataParam("path") InputStream uploadedInputStream,
+	    		@FormDataParam("path") FormDataContentDisposition fileDetail,
+	    		@FormDataParam("user_name") String name,
+	    		@FormDataParam("dob") String dob) throws Exception{
+	     	    		 	
+	    	DatabaseConnection dc = new DatabaseConnection();
+	    	/*
+	    	 * Code to store image in path
+	    	 */
+	    	return null;
 	    }
 }
